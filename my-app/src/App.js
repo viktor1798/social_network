f66947d16eddd609e5e36f9c0093ff1f40674f25
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-13 02:55:43 +0300

    add react app, work check

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+     
+      <div>
+        Lets go Learn React
+        </div>
+    </div>
+  );
+}
+
+export default App;
