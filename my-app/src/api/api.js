42634792f97661ed9dab3d95cb6296c8e708bfb8
viktor1798 <vktrsmsk1798@gmail.com>
2023-02-22 23:18:20 +0300

    api

---
+++ my-app/src/api/api.js
@@ -0,0 +1,19 @@
+import Axios from "axios";
+
+const instance = Axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "53d0aeb3-4b9a-4a15-8fc3-134f293e1718",
+  },
+});
+
+export const usersAPI = {
+  getUsers(numberPage, pageSize) {
+    return instance
+      .get(`users?page=${numberPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
--- my-app/src/components/mainblock/main-content/Users/usersContainer.jsx
+++ my-app/src/components/mainblock/main-content/Users/usersContainer.jsx
@@ -8,29 +8,25 @@ import {
   setTotalCount,
   toogleIsFethcing,
 } from "../../../../redux/usersReduser";
-import Axios from "axios";
 import Users from "./users";
 import Preloader from "../../../common/preloader";
+import { usersAPI } from "../../../../api/api";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.toogleIsFethcing(true);
-    Axios.get("https://social-network.samuraijs.com/api/1.0/users",{withCredentials:true}).then(
-      (response) => {
-        this.props.toogleIsFethcing(false);
-        this.props.setUsers(response.data.items);
-        this.props.setTotalCount(response.data.totalCount);
-      }
-    );
-  }
+     usersAPI.getUsers(this.props.numberPage, this.props.pageSize).then(data=>{
+      this.props.toogleIsFethcing(false);
+      this.props.setUsers(data.items);
+      this.props.setTotalCount(data.totalCount);
+     })
+}
   onPageChenged = (numberPage) => {
     this.props.setCurrentPage(numberPage);
     this.props.toogleIsFethcing(true);
-    Axios.get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${numberPage}&count=${this.props.pageSize}`,{withCredentials:true}
-    ).then((response) => {
+    usersAPI.getUsers(numberPage, this.props.pageSize).then(data=>{
       this.props.toogleIsFethcing(false);
-      this.props.setUsers(response.data.items);
+      this.props.setUsers(data.items);
     });
   };
 
